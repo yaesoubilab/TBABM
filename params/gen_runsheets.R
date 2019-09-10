334e0ec8ecb32506b966b102295db918fb533a3b
Marcus Russi <marcusrussi@gmail.com>
2019-09-10 16:58:13 -0400

    Refactor of gen_runsheets.R and addition of buggy uniform prior sampling feature. New arg parsing

--- params/gen_runsheets.R
+++ params/gen_runsheets.R
@@ -8,6 +8,36 @@ library(tools, quietly=TRUE)
 library(jsonlite, quietly=TRUE, warn.conflicts=FALSE)
 library(optparse, quietly=TRUE)
 
+tryInform <- function(code, message) {
+  handleError <- function(c) {
+    c$message <- paste0(c$message, "\n", '(', message, ')')
+    stop(c)
+  }
+
+  tryCatch(code, error=handleError)
+}
+
+die <- function(message) {
+  write(paste0("Error: ", message), stderr())
+  quit(status=1)
+}
+
+option_list <- list(
+  make_option(c("-p", "--prior"),
+              action="store_true",
+              default=FALSE,
+              help="Create a collated runsheet from uniform prior distributions"),
+  make_option(c("-r", "--range"),
+              action="store_true",
+              default=FALSE,
+              help="Create a collated runsheet from the cartesian product of sequences"),
+  make_option(c("-n", "--num-samples"),
+              action="store",
+              default=0,
+              help="(-p only) Number of samples from joint prior"))
+
+usage <- "%prog [options] PROTOTYPE_FILE RANGEFILE/PRIORFILE"
+description <- "Creates runsheets from either rangefiles or priorfiles"
 
 # Spec for the RunSheet
 runsheet_spec <- cols(
@@ -26,8 +56,8 @@ runsheet_spec <- cols(
 # Spec for the priorfile
 priorfile_spec <- cols(
   name  = col_character(),
-  lower = col_double(),
-  upper = col_double()
+  min   = col_double(),
+  max   = col_double()
 )
 
 # Spec for the rangefile
@@ -37,6 +67,16 @@ rangefile_spec <- cols(
   upper = col_double(),
   step  = col_double()
 )
+
+GenSamples <- function(input, n) {
+  tp <- transpose(input) %>% setNames(input$name)
+  map(tp, ~runif(n, .$min, .$max))
+}
+
+GenSamples_df <- function(GenSamples_output, n) {
+  mutate(as_tibble(GenSamples_output), run.id=1:n) %>%
+    select(run.id, everything())
+}
 
 # Cross all of the different parameter combinations together. The result of
 # this call is an unkeyed list of lists, where the innermost lists contain k-v
@@ -54,8 +94,7 @@ GenCombinations <- function(substitutions) {
 # fname -> tibble containing a row for each run where the columns are
 # the various parameters
 GenCombinations_df <- function(rangefile_fname) {
-  substitutions <- read_csv(rangefile_fname, col_types=rangefile_spec) %>%
-    as.tibble()
+  substitutions <- read_csv(rangefile_fname, col_types=rangefile_spec)
 
   pmap(substitutions,
        function(name, lower, upper, step) seq(lower, upper, step)) %>%
@@ -65,14 +104,29 @@ GenCombinations_df <- function(rangefile_fname) {
        select(run.id, everything())
 }
 
-GenRunSheets <- function(proto_fname, rangefile_fname) {
+# Given a list of substitutions, outputs a new parameter sheet, as a tibble, which
+# includes these substitutions
+# 'substitutions" is a keyed list, containing 'short-name':'parameter-1' pairs.
+# 
+# Right now, substitution can only be done on 'parameter-1', though this may change
+GenRunSheet <- function(substitutions, new_runsheet) {
+  slotter <- function(runsheet, name) {
+    row <- which(runsheet$`short-name` == name)
+    column <- 'parameter-1'
+    
+    new_runsheet[row, column] <- substitutions[[name]]
+    new_runsheet
+  }
+  
+  reduce(names(substitutions), slotter, .init=new_runsheet)
+}
+
+GenRunSheets_rf <- function(proto_fname, rangefile_fname) {
   
   # Read the prototype file and the substitutions file in and convert them to
   # tibbles
-  proto <- read_csv(proto_fname, col_types=runsheet_spec) %>% as_tibble()
-
-  substitutions <- read_csv(rangefile_fname, col_types=rangefile_spec) %>% 
-    as_tibble()
+  proto         <- read_csv(proto_fname,     col_types=runsheet_spec)
+  substitutions <- read_csv(rangefile_fname, col_types=rangefile_spec)
   
   # Cross all of the different parameter combinations together. The result of
   # this call is an unkeyed list of lists, where the innermost lists contain k-v
@@ -82,85 +136,23 @@ GenRunSheets <- function(proto_fname, rangefile_fname) {
   # now, are what 'parameter-1' should be set to.
   crossed <- GenCombinations(substitutions)
   
-  # Given a list of substitutions, outputs a new parameter sheet, as a tibble, which
-  # includes these substitutions
-  # 'substitutions" is a keyed list, containing 'short-name':'parameter-1' pairs.
-  # 
-  # Right now, substitution can only be done on 'parameter-1', though this may change
-  GenRunSheet <- function(substitutions, new_runsheet) {
-    for (name in names(substitutions)) {
-      row <- which(new_runsheet$`short-name` == name)
-      column <- 'parameter-1'
-      
-      new_runsheet[row, column] <- substitutions[[name]]
-    }
-    
-    new_runsheet
-  }
-  
-  crossed %>% map(~GenRunSheet(., proto))
+  map(crossed, GenRunsheet, proto)
 }
 
-WriteRunSheets <- function(runsheets, prefix="RunSheet_") {
+GenRunSheets_pf <- function(proto_fname, priorfile_fname, n) {
   
-  # Determine the number of digits needed to represent the ID of the
-  # last RunSheet
-  num_sheets <- length(runsheets)
-  num_digits <- floor(log10(num_sheets)) + 1
+  # Read the prototype file and the substitutions file in and convert them to
+  # tibbles
+  proto  <- read_csv(proto_fname,     col_types=runsheet_spec)
+  priors <- read_csv(priorfile_fname, col_types=priorfile_spec)
   
-  WriteRunSheet <- function(runsheet, sheet_id) {
-    print(runsheet)
-    
-    # Format the RunSheet ID: add an appropriate amount of leading 0's
-    number <- formatC(sheet_id, width=num_digits, format="d", flag="0")
-    
-    # Generate paths to write .csv and .json file to
-    path_csv <- paste0(prefix, number, ".csv")
-    path_json <- paste0(prefix, number, ".json")
-    
-    # Write the .csv and .json files to disk. It's important to make sure NA is
-    # represented as the empty string, so that an empty cell is represented as
-    # ",,".
-    write_json(runsheet, path_json, na='null')
-  }
-  
-  map2(runsheets, seq(num_sheets), WriteRunSheet)
-}
+  sampled <- GenSamples(priors, n)
 
-tryInform <- function(code, message) {
-  handleError <- function(c) {
-    c$message <- paste0(c$message, "\n", '(', message, ')')
-    stop(c)
-  }
-
-  tryCatch(code, error=handleError)
-}
-
-die <- function(message) {
-  write(paste0("Error: ", message), stderr())
-  quit(status=1)
+  list(dfs = map(pmap(sampled, list), GenRunSheet, proto),
+       vs  = sampled)
 }
 
 main <- function(args) {
-
-  option_list <- list(
-    make_option(c("-p", "--prior"),
-                action="store_true",
-                default=FALSE,
-                help="Create a collated runsheet from uniform prior distributions"),
-    make_option(c("-r", "--range"),
-                action="store_true",
-                default=FALSE,
-                help="Create a collated runsheet from the cartesian product of sequences"),
-    make_option(c("-n", "--num-samples"),
-                action="store",
-                default=0,
-                help="(-p only) Number of samples from joint prior")
-  )
-  
-  usage <- "%prog [options] PROTOTYPE_FILE RANGEFILE/PRIORFILE"
-  description <- "Creates runsheets from either rangefiles or priorfiles"
-
   parser <- OptionParser(option_list=option_list,
                          usage=usage,
                          prog="./gen_runsheets.R",
@@ -171,34 +163,42 @@ main <- function(args) {
 
   if (opts$prior & opts$range)
     die("Can't specify prior and range at the same time")
-
   if (! (opts$prior || opts$range))
     die("Must specifiy either -p or -rn")
-
   if (opts$prior && !opts$`num-samples`)
     die("When using -p, must specificy number of samples")
+  if (length(args) != 2)
+    die("Must specify PROTOTYPE_FILE RANGEFILE/PRIORFILE")
 
-  print(opts)
-  proto_fname     <- args[1]
-  rangefile_fname <- args[2]
+  proto_fname <- args[1]
+  file_fname  <- args[2]
   
-  GenPrefix <- function(rangefile_fname) return("")
-  
-  runsheets <- GenRunSheets(proto_fname, rangefile_fname)
-  
-  collate <- TRUE
-  if (collate) {
-    map(runsheets, ~toJSON(., na='null')) %>% 
-      paste(collapse="\n") %>% 
-      write(file="runsheets.json")
-    
-    # iwalk(runsheets, ~write_csv(.x, "runsheets.csv", append=(.y != 1)))
-  } else {
-    WriteRunSheets(runsheets, prefix="")
+  if (opts$range)
+    runsheets <- GenRunSheets_rf(proto_fname, file_fname)
+  else if (opts$prior) {
+    runsheets <- GenRunSheets_pf(proto_fname,
+                                 file_fname,
+                                 opts$`num-samples`)
+    vs        <- runsheets$vs
+    runsheets <- runsheets$dfs
   }
-  
-  write_csv(GenCombinations_df(rangefile_fname), 'runsheets.csv')
+
+  writer <- function(runsheet) {
+    json <- toJSON(runsheet)
+    write(paste0(json, "\n"),
+          file="runsheets.json",
+          append=TRUE)
+  }
+
+  tryInform(walk(runsheets, writer), "Writing of collated runsheet failed")
+
+  if (opts$range)
+    tryInform(write_csv(GenCombinations_df(file_fname), 'runsheets.csv'),
+              "Writing of runsheet table failed")
+  else
+    tryInform(write_csv(GenSamples_df(vs, opts$`num-samples`),
+                        'runsheets.csv'),
+              "Writing of runsheet table failed")
 }
 
 main(commandArgs(trailingOnly=TRUE))
-
