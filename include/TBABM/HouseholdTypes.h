c3ca9ee5143e7dc01cee11c0076a915a58aa094d
Marcus Russi <marcusrussi@gmail.com>
2019-11-13 16:15:08 -0500

    Include of previously .gitignore'd HouseholdTypes.h

---
+++ include/TBABM/HouseholdTypes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct {
+  int cases_found;
+  int cases_found_hiv;
+  int cases_found_children;
+  int screenings;
+  int screenings_hiv;
+  int screenings_children;
+} ContactTraceResult;
+
